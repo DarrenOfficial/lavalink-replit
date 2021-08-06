96bf55424bccf71dd146b671848c9a1a31bcd820
Darren <darrennathanaelofficial@gmail.com>
2021-08-06 16:24:14 -0400

    owo whats this

---
+++ start.sh
@@ -0,0 +1,6 @@
+## install jabba and sht
+curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
+## get java 13
+jabba install openjdk@1.13.0-2
+npm i
+node index
