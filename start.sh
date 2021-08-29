b608508ded336f2dcaaed21a60d1bb303d920901
Darren <darrennathanaelofficial@gmail.com>
2021-08-29 12:36:21 -0400

    yaya update to latest because compatibility garbag

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 ## install jabba and sht
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
-## get java 13
-jabba install openjdk@1.13.0-2
+## get java 16
+jabba install zulu@1.16.0
 npm i
 node index
