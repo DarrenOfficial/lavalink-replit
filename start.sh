27914aa5923077a48b004de2644968f382a763be
Darren <github@darrennathanael.com>
2021-11-10 05:20:58 -0500

    info?

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 ## install jabba and sht
+echo Downloading Java 16...
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
 ## get java 16
 jabba install zulu@1.16.0
+curl Java is installed.
+java -version
 npm i
+echo Donwloading lavalink jar...
 node index
