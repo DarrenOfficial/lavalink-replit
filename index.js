839abb3f02c117a2457faead81226f72d871d08f
Darren <github@darrennathanael.com>
2021-11-28 22:02:31 -0500

    code cleanup 2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { default: { stream } } = require("got");
 const { createWriteStream } = require("fs"); 
 const { execSync } = require("child_process");
-const url = "https://cdn.darrennathanael.com/jars/Lavalink.jar"; // This will always pull the latest with soundcloud fix ect ect
+const url = "https://cdn.darrennathanael.com/jars/Lavalink.jar"; // This will always pull the latest.
 const start = () => {
     const download = stream(url).pipe(createWriteStream('Lavalink.jar'));
     download.on("finish", () => {
