10f79978b5faf92de6b03dc54e5276ec7aa26629
JotaroKujo0525 <cloudandreibacor@gmail.com>
2022-08-08 06:53:45 +0800

    Nix experimental

--- start.sh
+++ start.sh
@@ -1,9 +1,3 @@
-## install jabba and sht
-echo Downloading Java 16...
-curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
-## get java 16
-jabba install zulu@1.16.0
-echo Java is installed.
 java -version
 npm i
 node index
