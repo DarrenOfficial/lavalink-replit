846d9d3b543950b0b47a6b4d04630d5501221cc8
Darren Nathanael <github@darrennathanael.com>
2024-03-18 08:00:05 +0000

    403 fixes

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const { createWriteStream } = require("fs");
 const { execSync } = require("child_process");
 const path = "./Lavalink.jar";
 
-// Based on https://github.com/Cog-Creators/Lavalink-Jars/, untill that koltin based one is fixed.
-const lljar = "https://cdn.darrennathanael.com/jars/cogs/Lavalink.jar";
-// temp code untill I made jar options & checking.
-// aka selection between downloading the "custom" "dev" "dev-with-plugins"
+// Always pull latest v3
+const lljar = "https://cdn.darrennathanael.com/jars/Lavalink.jar";
 
 const start = () => {
      const download = stream(lljar).pipe(createWriteStream('Lavalink.jar'));
